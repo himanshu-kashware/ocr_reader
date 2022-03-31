b3b0f0569a672c08f1d1051f2fc11f5d6149e35e
himanshu-kashware <himanshu@kashware.com>
2022-03-31 16:40:45 +0530

    update

--- server.py
+++ server.py
@@ -55,7 +55,8 @@ async def upload_file():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 async def get_document_id(path):
